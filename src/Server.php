956005d48c0684c8c5424158b64433898df9d211
Mikk Tendermann <mikk150@gmail.com>
2015-10-31 18:06:36 +0200

    cleanup for quieter operation

--- src/Server.php
+++ src/Server.php
@@ -24,20 +24,12 @@ class Server extends EventEmitter
             $emit=true;
             $conn->on('data', function ($data, $conn) use (&$emit) {
                 $event=strtolower(substr($data, 0, 4));
-
-                echo '> '.$data;
                 if ($emit) {
                     $this->emit($event, [$data, $conn, &$emit]);
                 }
             });
-            // echo 'new connection'.PHP_EOL;
-
-            // $parser = new RequestParser();
-
-            // $conn->on('data', array($parser,'feed'));
 
             $conn->write('220 HELLO M8!'.PHP_EOL);
-            // //$conn->write('421 BYE M8!'.PHP_EOL);
         });
     }
 }
